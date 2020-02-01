198e650e68f3b2069ec6fe430dd003d064c002df
jhlim <jhlym92@gmail.com>
2020-02-01 20:21:40 +0900

    Updates

---
+++ precache-manifest.03216308c44e7096f8ed8cef8e42be80.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7441502a4c0f59c0b24f34c57575b591",
+    "url": "/clone-wanted/index.html"
+  },
+  {
+    "revision": "a5569395ab4c1a388734",
+    "url": "/clone-wanted/static/css/2.12d83a48.chunk.css"
+  },
+  {
+    "revision": "9baece355db76553f8b8",
+    "url": "/clone-wanted/static/css/main.d31197ce.chunk.css"
+  },
+  {
+    "revision": "a5569395ab4c1a388734",
+    "url": "/clone-wanted/static/js/2.c6487409.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/clone-wanted/static/js/2.c6487409.chunk.js.LICENSE"
+  },
+  {
+    "revision": "411a6c6c8e07e333497a",
+    "url": "/clone-wanted/static/js/3.2d4c82cb.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/clone-wanted/static/js/3.2d4c82cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f110a3ef54d3e8e18cf9",
+    "url": "/clone-wanted/static/js/4.b81803d2.chunk.js"
+  },
+  {
+    "revision": "9baece355db76553f8b8",
+    "url": "/clone-wanted/static/js/main.72d5e5b6.chunk.js"
+  },
+  {
+    "revision": "b8cea780098e60d7a1fb",
+    "url": "/clone-wanted/static/js/runtime-main.e688230e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/clone-wanted/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/clone-wanted/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/clone-wanted/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/clone-wanted/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/clone-wanted/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/clone-wanted/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/clone-wanted/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/clone-wanted/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/clone-wanted/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/clone-wanted/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/clone-wanted/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/clone-wanted/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/clone-wanted/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/clone-wanted/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/clone-wanted/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/clone-wanted/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.252e0cf4ca6b5efebbc964a6c3f5c82a.js"
+  "/clone-wanted/precache-manifest.03216308c44e7096f8ed8cef8e42be80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clone-wanted/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
